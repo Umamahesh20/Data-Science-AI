4f772784733036c347da1caf4f229f514d163905
Umamaheswararao Nallabolu <88869589+Umamahesh20@users.noreply.github.com>
2026-07-18 16:32:01 +0530

    Update datatype2.py

--- python/datatype2.py
+++ python/datatype2.py
@@ -1,16 +1,16 @@
-#set1=(1,2,3,4,5,1,2,3,4,5)
-#print(set1)
+set1=(1,2,3,4,5,1,2,3,4,5)
+print(set1)
 
 #unordered:
 
-#names=('Umamahesh','sumanth','Venketash')
-#print(names)
-#print(hash('sumanth'))
-#note: hash() is the function we use to know the hash value of n object
+names=('Umamahesh','sumanth','Venketash')
+print(names)
+print(hash('sumanth'))
+note: hash() is the function we use to know the hash value of n object
 
 
-#details = {'name':'vyshu','age':'21','place':'wargal','prof':'devloper'}
-#print(details)
+details = {'name':'vyshu','age':'21','place':'wargal','prof':'devloper'}
+print(details)
 
 #frozenset
 
@@ -23,25 +23,25 @@ list
 
 
 #object:
-#num1=10
+num1=10
 #values
-#print(num1)
+print(num1)
 #type
-#print (type(num1))
+print (type(num1))
 #id
-#print(id(num1))
+print(id(num1))
 
 
 
-#list1=['python','java','Data Science','Ai']
-#print(list)
+list1=['python','java','Data Science','Ai']
+print(list)
 #type
-#print(type(list1))
+print(type(list1))
 #id
-#print(id(list1))
+print(id(list1))
 
-#def addition():
- #   a=10
- #   b=4
-  #  a+b
-   # print(addition())
+def addition():
+   a=10
+   b=4
+  a+b
+ print(addition())
