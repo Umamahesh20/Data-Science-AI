d8d946470e0e09d120efeabf76bb7eb758c38cf7
Umamahesh20 <numamaheswararao20.com>
2026-07-27 22:35:37 +0530

    update day 10 python codes

---
+++ python/D10Operators.py
@@ -0,0 +1,20 @@
+#Bitwise operators:
+#Bitwise and(&):
+print(3&4)
+
+#Bitwise or(|):
+print(3|4)
+
+#Bitwise xor(^):
+print(3^4)
+
+#Bitwise not(~):
+print(~3)
+
+#Note: computers doesn't store negative numbers in binary format, so the output of the bitwise not operator is -4.
+
+#Bitwise left shift(<<):
+print(3<<1)
+
+#Bitwise right shift(>>):
+print(3>>1)
