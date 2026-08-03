b3eeec603c8ca9e496907c26e2e666bd1e460d99
Umamahesh20 <numamaheswararao20.com>
2026-08-03 18:56:16 +0530

    update day 12 python codes

---
+++ python/D12tuplepackingnunpacking.py
@@ -0,0 +1,37 @@
+#packing
+num1=10,20,39
+
+print(num1)
+
+#unpacking:
+# tuple1=('M1','Python','java','DBMS')
+# sub1,sub2sub3,sub4=tuple1
+
+# print (sub3)
+
+# names=['sumantha','sumanth','teja']
+# name,*remNames,lastname=names
+# print(names)
+# print(remNames)
+# print(lastname)
+
+str1='venket'
+str2='narayana@143'
+print(str1+str2)
+
+triner='sumanth Teja'
+print(triner)
+print('='*35)
+print('teja weds teja')
+print('='*35)
+print('Venue:inkollu')
+
+#going to venket sir class
+
+raining='no'
+if raining=='yes':
+    print('h')
+else:
+    print('Thadava kuda podham')
+
+age=int(input())
---
+++ python/D13Condtionalstatement.py
@@ -0,0 +1,75 @@
+# # #write a program to check whether a number even or odd
+
+# # num = int(input('enter a number: '))
+# # if num % 2 == 0:
+# #     print('number is even')
+# # else:
+# #     print('number is odd')
+
+
+# # ## checking wether a password is lenthy enough or not
+# # password = input('Enter your password: ')
+# # if len(password) >= 8:
+# #     print('password has enough characters')
+# # else:
+# #     print('Not lengthy enough')
+
+# #write a program to withdraw money from from my account
+
+# withdraw=int(input('enter the aount to withdraw:'))
+# balance=10000
+# if withdraw<=balance:
+#     balance-=withdraw
+#     print(f'withdraw successfull and current balance is {balance}')
+# else:
+#     print('Not enough balance')
+
+# #write a program to check whether a person can login or not
+# password=input('enter your password: ')
+# savedpassword='Sowmya@752'
+# if password==savedpassword:
+#     print('login successfully')
+# else:
+#     print('login failed')
+
+# ##write a program to check whether a person is eligible for loan
+# #or not based on his salary
+
+# #
+# #write a program whether a person is eligible to login or not
+
+# empIds=[101,102,103,104,105]
+# fingerPrint=input('Enter whether the finger print is vaild or not')
+# empId=int(input('Enter empolyee id'))
+# if fingerPrint=='valid' and empId in empIds:
+#     print('Login successfull')
+# else:
+#     print('emploee does not exist')
+
+# #write a program to check loan eligibility based on salary and cibil score
+
+# salary=int(input('Enter the salary:'))
+# cibil=int(input('Enter cibil score:'))
+# if salary>=60000 and cibil>=7000:
+#     print('Customer is eligible for loan')
+# else:
+#     print('Coustomer did not match the loan eligibility criteria')
+
+# #or operator
+# graduation=input('Enter your qualification:')
+# if graduation=='degree' or graduation=='Btech':
+#     print('You are meeting the application criteria')
+# else:
+#     print('job criteria did not match')
+
+
+# #checking whether a person has required criteria for a suitable groom
+# #property=2 crores or must be a goverment employee
+# proprtty=int(input('Enter the total property in terms of crores:'))
+# employment=input('Enter your employement type:')
+# if property >=2 or  employment: # type: ignore
+#     print('Suitable groom found')
+# else:
+#     print('no groom with your requirement')
+    
+
