6a3713fa5d63d74980301c5cda0cf5abf858d875
Umamahesh20 <numamaheswararao20.com>
2026-09-23 15:00:03 +0530

    updating codes

--- python/problesolving23-09-26.py
+++ python/problesolving23-09-26.py
@@ -1,14 +1,12 @@
-# To swap first and last characters of a word
-def swapFAL():
-    word = input("Enter a word: ")
-    if len(word) < 2:
-        return word    
-    chars = list(word)
-    
-    chars[0], chars[-1] = chars[-1], chars[0]
-    return "".join(chars)
-print(swapFAL())
-
+#1. Count Characters Count the number of characters in a string without using len(). 
+# Input: "hello" Output: 5
+s=input("Enter a String: ")
+c=0
+for char in s:
+    c+=1
+print("Number of characters:", c)
+# 2. Count Vowels Count the number of vowels (a, e, i, o, u) in a string. 
+# Input: "education" Output: 5
 def count_vowels_consonants(s):
     vowels = 0
     consonants = 0
@@ -24,3 +22,5 @@ def count_vowels_consonants(s):
     print("Consonants:", consonants)
 s = input("Enter a string: ")
 count_vowels_consonants(s)
+#3. Count Consonants Count the consonants in a string.
+#  Input: "python" Output: 5
